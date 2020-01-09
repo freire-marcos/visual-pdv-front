319b742fd944e858c84a3dd9f8ce2209a026428b
freire-marcos <marcosfreire.dev@gmail.com>
2020-01-08 23:21:29 -0300

    Primeiro commit com algumas alterações iniciais

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+
+export interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+
+export const ROUTES: RouteInfo[] = [
+    { path: '/dashboard',     title: 'Dashboard',         icon:'nc-bank',       class: '' },
+    { path: '/vendas',        title: 'Vendas',            icon:'nc-cart-simple',class: '' },
+    { path: '/estoque',       title: 'Estoque',           icon:'nc-box-2',      class: '' },
+    { path: '/financeiro',    title: 'Financeiro',        icon:'nc-money-coins',class: '' },
+    { path: '/icons',         title: 'Icons',             icon:'nc-diamond',    class: '' },
+    { path: '/maps',          title: 'Maps',              icon:'nc-pin-3',      class: '' },
+    { path: '/notifications', title: 'Notifications',     icon:'nc-bell-55',    class: '' },
+    { path: '/usuario',       title: 'Usuário',           icon:'nc-single-02',  class: '' },
+    { path: '/table',         title: 'Table List',        icon:'nc-tile-56',    class: '' },
+    { path: '/typography',    title: 'Typography',        icon:'nc-caps-small', class: '' }
+];
+
+@Component({
+    moduleId: module.id,
+    selector: 'sidebar-cmp',
+    templateUrl: 'sidebar.component.html',
+})
+
+export class SidebarComponent implements OnInit {
+    public menuItems: any[];
+    ngOnInit() {
+        this.menuItems = ROUTES.filter(menuItem => menuItem);
+    }
+}
